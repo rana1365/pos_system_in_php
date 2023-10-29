e6b6f326dc51c10430b0eb5349e13c176c66f81d
MD SOHEL RANA <rana135105@gmail.com>
2023-10-30 00:28:54 +0600

    Increment & Decrement Js snippet fixed

--- admin/assets/js/custom.js
+++ admin/assets/js/custom.js
@@ -19,12 +19,12 @@ $(document).ready( function () {
 
         var $quantityInput = $(this).closest('.qtyBox').find('.qty');
         var productId = $(this).closest('.qtyBox').find('.prodId');
-        var currentValue = parseInt($quantityInput.value());
+        var currentValue = parseInt($quantityInput.val());
 
         if (!isNaN(currentValue) && currentValue > 1) {
 
             var qtyVal = currentValue - 1;
-            $quantityInput.value(qtyVal);
+            $quantityInput.val(qtyVal);
         }
 
     });
